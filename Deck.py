48e707b2c07c976f4faf71f2323f2b92b5010016
Andre Krepel <dedekrepel@gmail.com>
2025-04-26 17:25:07 +0200

    first version of poker_game.py

--- Deck.py
+++ Deck.py
@@ -2,9 +2,17 @@ import random
 
 
 class Card:
+    '''
+    A class that represents a playing card with a suit and a rank.
+    '''
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["Clubs", "Spades", "Hearts", "Diamonds"]
     def __init__(self, suit, rank):
+        '''
+         Initialize (create and set up) a Card object.
+        :param suit: the suit of the card (must be in SUITS listt)
+        :param rank: the rank of the card (must be in RANKS list)
+        '''
         if rank not in self.RANKS:
             raise ValueError("Invalid Rank")
         if suit not in self.SUITS:
@@ -13,39 +21,83 @@ class Card:
         self._rank = rank
 
     def __eq__(self,other):
+        '''
+        Check if two cards have the same rank.
+        :param other: another Card object
+        :return: True if ranks are the same, False otherwise
+        '''
         return self.rank == other.rank
 
     def __gt__(self, other):
+        '''
+        Compare two cards by their ranks.
+        :param other: another Card object
+        :return: True if this card is greater than self, False otherwise
+        '''
         return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
 
     def __str__(self):
+        '''
+        Represents the Card as a String
+        :return: rank and suit as a string
+        '''
         return f"{self._rank} of {self._suit}"
 
     def __repr__(self):
+        '''
+        Official string representation of the self card.
+        :return: same as __str__
+        '''
         return self.__str__()
 
     @property
     def suit(self):
+        '''
+        Getter (returns) for suit.
+        :return: the suit of the card
+        '''
         return self._suit
 
     @property
     def rank(self):
+        '''
+        Getter (returns) for rank.
+        :return: the rank of the card
+        '''
         return self._rank
 
 class Deck:
+    '''
+    A class that represents a deck of 52 playing cards.
+    '''
     def __init__(self):
+        '''
+        Initialize (create and set up) a full deck of 52 cards.
+        '''
         self._deck = []
         for suit in Card.SUITS:
             for rank in Card.RANKS:
                 self._deck.append(Card(suit,rank))
 
     def __str__(self):
+        '''
+        Represents the deck as a string
+        :return: string of all cards in the deck
+        '''
         return str(self._deck)
 
     def shuffle(self):
+        '''
+        Shuffle the deck randomly using random.
+        :return: the deck
+        '''
         random.shuffle(self._deck)
 
     def deal(self):
+        '''
+        Deal (remove and return) the top card of the deck.
+        :return: a Card object
+        '''
         return self._deck.pop(0)
 
 if __name__ == "__main__":
--- PokerGame.py
+++ PokerGame.py
@@ -1,7 +1,14 @@
 from Deck import Deck, Card
 
 class PokerHand:
+    '''
+    A class that represents a poker hand with 5 cards.
+    '''
     def __init__(self, deck):
+        '''
+        Initialize (create and set up) a PokerHand object.
+        :param deck: a Deck object to deal cards from
+        '''
 
         cards = []
         for i in range(5):
@@ -10,13 +17,25 @@ class PokerHand:
 
     @property
     def cards(self):
+        '''
+        Getter (returns) for the list of cards in the hand.
+        :return: list of 5 Card objects
+        '''
         return self._cards
 
     def __str__(self):
+        '''
+        Represents the poker hand as a string
+        :return: string listing the 5 cards
+        '''
         return str(self.cards)
 
     @property
     def is_flush(self):
+        '''
+        Check if all cards have the same suit (check if its a flush).
+        :return: True if hand is a flush, False otherwise
+        '''
         for card in self.cards[1:]:
             if self.cards[0].suit != card.suit:
                 return False
@@ -24,11 +43,19 @@ class PokerHand:
 
     @property
     def is_full_house(self):
+        '''
+        Check if hand is a full house (3 of a kind + a pair).
+        :return: True if full house, False otherwise
+        '''
         return self.number_matches == 8
 
 
     @property
     def number_matches(self):
+        '''
+        Calculate the total number of matching pairs between cards.
+        :return: number of pairs
+        '''
         matches = 0
         for i in range(len(self.cards)):
             for j in range(len(self.cards)):
@@ -41,6 +68,10 @@ class PokerHand:
 
     @property
     def is_pair(self):
+        '''
+        Check if hand has exactly one pair.
+        :return: True if exactly one pair, False otherwise
+        '''
         if self.number_matches == 2: #simple
             return True
         return False
@@ -48,22 +79,38 @@ class PokerHand:
 
     @property
     def is_two_pair(self):
+        '''
+        Check if hand has exactly two pairs.
+        :return: True if two pairs, False otherwise
+        '''
         return self.number_matches == 4 #more advanced
 
     @property
     def is_trips(self):
+        '''
+        Check if hand has three of a kind.
+        :return: True if trips, False otherwise
+        '''
         if self.number_matches == 6:
             return True
         return False
 
     @property
     def is_quads(self):
+        '''
+        Check if hand has four of a kind.
+        :return: True if quads, False otherwise
+        '''
         if self.number_matches == 12:
             return True
         return False
 
     @property
     def is_straight(self):
+        '''
+        Check if hand has 5 cards in sequence (straight).
+        :return: True if straight, False otherwise
+        '''
         self.cards.sort()
         distance = Card.RANKS.index(self.cards[4].rank) - \
                    Card.RANKS.index(self.cards[0].rank)
